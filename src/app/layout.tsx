c8896eb1933319f3bc4dd2fd46ddbf76572ae722
Thyon3 <asnakemengesha80@gmail.com>
2026-01-12 04:26:57 -0500

    Setup root layout with Poppins font

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${font.className}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
